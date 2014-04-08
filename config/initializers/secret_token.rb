fe59b61175f91bc19a8413a5af1269f3ae64b102
Arjun Rao <a.rao456@gmail.com>
2014-04-07 17:42:35 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SolveIt::Application.config.secret_key_base = '8d85e8f9b0597b3b64c2673c3537f9251c981b5727e4fdcd7f891abee0713ed2d32d7abbd0eb37e2466ffc693437a0923e4a4b1bc0253a5adeff7b1a99e3f2b6'
