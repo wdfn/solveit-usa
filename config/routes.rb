ffb187507773420378b481325f28b7ab3117f4c4
leehanna <hannallee@berkeley.edu>
2014-04-27 00:18:46 -0700

    delete and sign_out fixed

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ SolveIt::Application.routes.draw do
   # We ask that you don't use the :as option here, as Forem relies on it being the default of "forem"
   mount Forem::Engine, :at => '/forums'
 
-  # devise_for :views
+  devise_for :views
   # This line mounts Forem's routes at /forums by default.
   # This means, any requests to the /forums URL of your application will go to Forem::ForumsController#index.
   # If you would like to change where this extension is mounted, simply change the :at option to something different.
